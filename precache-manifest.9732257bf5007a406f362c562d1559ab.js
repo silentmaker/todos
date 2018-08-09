1e6cf139f936091c9099269831477c9e07327c7d
silentmaker <demonmaxc@gmail.com>
2018-08-09 17:50:32 +0800

    deploy

---
+++ precache-manifest.9732257bf5007a406f362c562d1559ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/todos/robots.txt"
+  },
+  {
+    "revision": "5c1091f631e3f42be45f",
+    "url": "/todos/js/chunk-vendors.5c1091f6.js"
+  },
+  {
+    "revision": "aea9ae9d2900b68aa74b",
+    "url": "/todos/js/app.aea9ae9d.js"
+  },
+  {
+    "revision": "dc20c20e8e94424ab3df2389f33ce5fc",
+    "url": "/todos/index.html"
+  },
+  {
+    "revision": "aea9ae9d2900b68aa74b",
+    "url": "/todos/css/app.25f89e30.css"
+  }
+];
